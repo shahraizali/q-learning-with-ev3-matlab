a71cd7786526e7d237deb769f01fea754f752022
shahraiz ali <shahraizali10@yahoo.com>
2017-05-07 13:55:51 +0500

    added comments

--- lineq.m
+++ lineq.m
@@ -1,26 +1,45 @@
 clear
+
+% declaring and initializing variables to store values
+
+%state of sensor
 state = 0;
+
+%action to perform
 action = 0;
+
+% the next state
 nextState = 0;
+
+% r is reward variable
 r = 0;
+
+% the main Q matrix
 QVALUES = [0 0 0 ; 0 0 0 ];
+
 randomNum = 0;
 max = 0;
-
 c= 0;
 
- alpha = 0.2;     
+% Alpha and discount are used in Q formula acording to algorithm
+alpha = 0.2;     
+discount = 0.5;		
 
-        discount = 0.5;		
+exploration = 2; 
 
-	exploration = 2; 
-
+% connecting to lego kit using usb 
+% this could be done using bluetooth
 mylego =  legoev3('usb');
 
+
+%checking the connection
 mylego.beep();
 
+% setting up color sensor and assigning 'color' variable to use in code
+% 1 in the arguement as the port number
 color  =  colorSensor(mylego , 1);
 
+% setting up both left and right motors to port A and B respectively
 leftMotor = motor( mylego , 'A');
 leftMotor.Speed = 80;
 
@@ -30,11 +49,18 @@ rightMotor.Speed = 80;
 
 %stop_motor(leftMotor , rightMotor);
 
+%There is no main function in here this while loop is an always true loop
+% all the function logic is in it
+
  while(true)
+	% getting the current state i.e based on current sensor value 1 for black 2 otherwise
     state =  getState(color);
-    max  =  -32000;
+   % setting max to a low value so that first time its always less than default i.e 0
+	max  =  -32000;
     action = 3;
     
+	
+	% Getting appropriate action from the Q matrix
     for c = 1:3
         if(QVALUES(state , c) > max)
             max  =  QVALUES(state , c);
@@ -44,13 +70,18 @@ rightMotor.Speed = 80;
     
     randNum = rand(3);
     
-    
+    % executing the action 
     execute(action , state , leftMotor , rightMotor);
     
+	% getting next state
     nextState = getState(color);
-    r = getReward(nextState);
+    %calculate the reward for next state
+	r = getReward(nextState);
+	
       max = -32000;
       
+	  
+	  % updating the q matrix
       for c = 1:3
           if(QVALUES(nextState,c) > max)
               max = QVALUES(nextState,c);
@@ -58,10 +89,13 @@ rightMotor.Speed = 80;
           QVALUES(state,action) = (QVALUES(state,action)) + alpha * (r + (discount * max) - QVALUES(state,action));
       end
       
+	  
+	  % and finally loop back to getting current state again
+	  
  end
  
 
-
+% Implementing Forward Motion 
 function move_forward(left , right , speed )
     left.Speed =  speed;
     right.Speed =  speed;
@@ -72,12 +106,14 @@ function move_forward(left , right , speed )
     stop_motor(left , right)
 end
 
+% Stoping both motors 
 function stop_motor(left , right)
     stop(left);
     stop(right);
 end
 
 
+% Implementing left turning
 function turn_left(left , right )
 
  for i =0:2
@@ -90,6 +126,8 @@ function turn_left(left , right )
     stop_motor(left , right);
 end
 
+
+% Implementing right turning
 function turn_right(left , right )
 
       for i =0:2 
@@ -101,6 +139,8 @@ function turn_right(left , right )
     stop_motor(left , right);
 end
 
+
+% Implementing reverse
 function goback(left , right , j)
     left.Speed =  -80;
     right.Speed = -80;
@@ -112,9 +152,13 @@ function goback(left , right , j)
 end
 
 
-
+% obtaining state on the base of value from the light sensor
+% this sensor reads value from 1 - 100 where values 1-15 
+% shows that color is black so if color is black state defined
+% is 1 other wise 2
 function state=getState(color)
    intensity =  readLightIntensity(color ,'reflected' );
+   % 15 refers to black color 
    if(intensity < 15 )
        state = 1;
    else
@@ -124,6 +168,8 @@ end
 
 
 
+% getting reward for certain state mostly the next states
+% are provided to it in order to check for best next option available
 
 function reward = getReward(state)
     if(state == 1)
@@ -135,6 +181,7 @@ function reward = getReward(state)
 end
 
  
+ % This function execute any action that is provided as arguement
  
  function execute(action , state , leftMotor , rightMotor)
     
